35d83b4bac9dd66332b99ada677466b94982496b
poku-e <crichardson@xclusivetradinginc.com>
2025-11-01 17:12:22 -0500

    Initial Commit

---
+++ Zenpane/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zenpane
+//
+//  Created by Corey Richardson on 11/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zenpane/ZenpaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenpaneApp.swift
+//  Zenpane
+//
+//  Created by Corey Richardson on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ZenpaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
